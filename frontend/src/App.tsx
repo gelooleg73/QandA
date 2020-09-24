2be37c6797a29771495bb8a8b58aa820cd7a5b28
gelooleg73 <oleg.kavchinskiy@gmail.com>
2020-09-24 17:15:50 +0300

    frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
+import { fontFamily, fontSize, gray2 } from './Styles';
+
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
+      <HomePage />
     </div>
   );
 };
--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -1,3 +1,14 @@
 import React from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import user from './user.svg';
-export const UserIcon = () => <img src={user} alt="User" width="12px" />;
+export const UserIcon = () => (
+  <img
+    src={user}
+    alt="User"
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+  />
+);
