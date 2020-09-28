23476aa524890108736ca03038d3483dfb647976
gelooleg73 <gelo@city-adm.rv.ua>
2020-09-28 23:27:36 +0300

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,41 @@ import React from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+import { NotFoundPage } from './NotFoundPage';
 
 const App: React.FC = () => {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        {/* <Route path="/" component={HomePage} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/ask" component={AskPage} />
+        <Route path="/signin" component={SignInPage} /> */}
+
+        <Switch>
+          <Redirect from="/home" to="/" />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/ask" component={AskPage} />
+          <Route path="/signin" component={SignInPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
